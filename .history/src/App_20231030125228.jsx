4a2c5bca17e19f551a3dceaca719538d5a72b6e2
logan-lampton <lampton.logan@gmail.com>
2023-10-30 14:04:12 -0400

    added tailwind, updating app page, added header

--- .history/src/App_20231030125228.jsx
+++ .history/src/App_20231030125228.jsx
@@ -7,23 +7,18 @@ function App() {
 
   return (
     <>
-      <div class="bg-gray-800 text-white font-bold py-2 px-4 border w-screen">
-        <h1>8 by 8 Club Manager</h1>
-        <button class="bg-neutral-100 hover:bg-neutral-50 text-black font-bold py-2 px-4 border neutral-100 rounded absolute top-5 right-5 h-15 w-100">
-          Log in
-        </button>
-      </div>
+      <h1>Starter Header</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        {/* <p>
+        <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
+        </p>
       </div>
-      {/* <p className='read-the-docs'>
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more!
-      </p> */}
+      </p>
     </>
   );
 }
