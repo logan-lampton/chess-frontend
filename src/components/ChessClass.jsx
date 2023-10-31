f665d40f4af2d1dc4afe667c5e37d653f6b6efcc
logan-lampton <lampton.logan@gmail.com>
2023-10-31 13:46:30 -0400

    added login component / hide homepage until user is logged in

--- src/components/ChessClass.jsx
+++ src/components/ChessClass.jsx
@@ -2,8 +2,11 @@ import react from 'react'
 
 function ChessClass() {
   return (
-    <div>
-      <p>Test</p>
+    <div class='border 2px gray-900'>
+      <div class='bg-gray-900 text-white font-bold py-2 px-4 border hover:bg-gray-700'>
+        <h2>Name of team</h2>
+      </div>
+      <p>map students here</p>
     </div>
   );
 }
