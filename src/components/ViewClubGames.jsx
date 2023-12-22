323b6fe8ad28d5e11ae275243ce4aa933472a3fa
logan-lampton <lampton.logan@gmail.com>
2023-12-21 20:25:45 -0500

    beginning to work on render in view games component

--- src/components/ViewClubGames.jsx
+++ src/components/ViewClubGames.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// club.inprogress
+// club.completed
 
 function ViewClubGames() {
+
+  const [activeGames, setActiveGames] = useState([]) 
+
   return (
-    <div>ViewClubGames</div>
+    <div className='border-2 border-gray-900'>
+      <div className='bg-gray-900 text-white font-bold py-2 px-4 border mb-4'>
+        Active Games
+      </div>
+    </div>
   )
 }
 
