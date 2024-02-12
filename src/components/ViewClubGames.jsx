939e32ffc13363df913652cae3fcc5be11d1d996
logan-lampton <lampton.logan@gmail.com>
2024-02-12 12:09:33 -0500

    formatting of ViewClubGames, starting Dropdown component

--- src/components/ViewClubGames.jsx
+++ src/components/ViewClubGames.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "../axiosConfig";
+import Dropdown from './Dropdown';
 
 function ViewClubGames() {
   const [games, setGames] = useState([]);
@@ -28,7 +29,7 @@ function ViewClubGames() {
 
   return (
     <div className='relative'>
-      <button className='absolute top-5 right-0 h-15 w-50 bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 border bg-gray-900 rounded mb-4'>
+      <button className='absolute top-5 right-10 h-15 w-50 bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 border bg-gray-900 rounded mb-4'>
         <Link
           // to={}
           className='text-white'
@@ -36,19 +37,25 @@ function ViewClubGames() {
           View Completed Games
         </Link>
       </button>
-      <div className='grid grid-cols-1 md:grid-cols-4 gap-4 my-20 mx-5w-screen'>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-8 my-20 mx-auto'>
         {games.map((game) => (
-          <div key={game.id} className='mt-5 mb-5'>
+          <div key={game.id} className='mt-5 mb-5 p-8'>
             <div className='border-2 border-gray-900'>
-              <div className='bg-gray-900 text-white font-bold py-2 px-4 border mb-2'>
-                <h3>
+              <div className='bg-gray-900 text-white font-bold py-2 px-4 border mb-2 flex justify-between'>
+                <h>
                   {game.players.white} / {game.players.black}
-                </h3>
+                </h>
+                <button className='text-black p-1 text-sm ml-2'>Cancel Game</button>
               </div>
-              <div className='ml-5'>
-                <p>White Player: {game.players.white}</p>
+              <div className='ml-5 p-1'>
+                <p className='mb-1'>White Player: {game.players.white}</p>
                 <p>Black Player: {game.players.black}</p>
-                <p>Winner: pending</p>
+                <div className='flex mt-3 mb-2'>
+                  <p className='mt-3'>Winner:</p>
+                  <div className='ml-2 align-middle'>
+                    <Dropdown />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
