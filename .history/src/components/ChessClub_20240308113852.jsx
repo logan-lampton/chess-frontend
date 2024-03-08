e2aef9cc5f208f49b474ebfec26a29de2c67869b
logan-lampton <lampton.logan@gmail.com>
2024-03-08 11:47:01 -0500

    fixed ChessClub load from AddStudent by adding a default for club state of any empty object

--- .history/src/components/ChessClub_20240308113852.jsx
+++ .history/src/components/ChessClub_20240308113852.jsx
@@ -19,7 +19,7 @@ function ChessClub() {
   const location = useLocation();
   const { club: initialClub } = location.state || {};
 
-  const [club, setClub] = useState(initialClub || {});
+  const [club, setClub] = useState(initialClub);
 
   const [confirmationPopUp, setConfirmationPopUp] = useState({
     message: "",
