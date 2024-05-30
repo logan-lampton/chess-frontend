3db2b8ba8822dc9c39a5492a974c0a367df4e17b
logan-lampton <lampton.logan@gmail.com>
2024-05-30 15:26:16 -0400

    functionality to save notes in lesson and push to backend

--- src/components/StudentScoreInput.jsx
+++ src/components/StudentScoreInput.jsx
@@ -1,49 +1,61 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-export default function StudentScoreInput({student, score, onScoreChange, max}) {
-    
-    const [isToggled, setIsToggled] = useState(false);
+export default function StudentScoreInput({ student, score, note, onScoreChange, onNoteChange, max }) {
+  const [isToggled, setIsToggled] = useState(false);
 
-    const handleScoreInputChange = e => {
-        const newScore = parseInt(e.target.value)
-        onScoreChange(student.id, newScore)
-    }
+  const handleScoreInputChange = e => {
+    const newScore = e.target.value;
+    onScoreChange(student.id, newScore);
+  };
 
-    const toggle = () => {
-      setIsToggled(prevState => !prevState)
-    }
+  const handleNoteChange = e => {
+    const newNote = e.target.value;
+    onNoteChange(student.id, newNote);
+  };
 
-    return (
-        <div className="">
-          <label htmlFor={`score-${student.id}`} className="block text-gray-700 p-2">
-            {student.name}
-          </label>
-          <div className="flex p-1">
-            <input
-              type="number"
-              id={`score-${student.id}`}
-              value={score}
-              onChange={handleScoreInputChange}
-              min="0"
-              max={max}
-              className="p-2 block w-100 shadow-sm border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 border-gray-400"
+  const toggle = () => {
+    setIsToggled(prevState => !prevState);
+  };
+
+  return (
+    <div className="">
+      <label htmlFor={`score-${student.id}`} className="block text-gray-700 p-2">
+        {student.name}
+      </label>
+      <div className="flex p-1">
+        <input
+          type="number"
+          id={`score-${student.id}`}
+          value={score}
+          onChange={handleScoreInputChange}
+          min="0"
+          max={max}
+          className="p-2 block w-100 shadow-sm border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 border-gray-400"
+        />
+        <p className="mt-2 ml-2 text-gray-500">/{max}</p>
+      </div>
+      {!isToggled ? (
+        <button className="mt-5 bg-gray-900 text-white py-2 px-4 rounded hover:bg-blue-700" onClick={toggle}>
+          Add Notes
+        </button>
+      ) : (
+        <div>
+          <div className="mt-5">
+            <textarea
+              style={{ resize: "none", height: "auto", minHeight: "2rem", width: "100%", overflowY: "hidden" }}
+              value={note}
+              onChange={handleNoteChange}
+              onInput={(e) => {
+                e.target.style.height = "auto";
+                e.target.style.height = (e.target.scrollHeight) + "px";
+              }}
             />
-            <p className="mt-2 ml-2 text-gray-500">/{max}</p>
           </div>
-          {!isToggled ? <button className="mt-5 bg-gray-900 text-white py-2 px-4 rounded hover:bg-blue-700" onClick={toggle}>Add Notes</button>
-          : 
-          <div>
-            <div className="mt-5">
-              <textarea style={{ resize: "none", height: "auto", minHeight: "2rem", width: "100%", overflowY: "hidden" }}
-                onInput={(e) => {
-                  e.target.style.height = "auto";
-                  e.target.style.height = (e.target.scrollHeight) + "px";
-                }}>
-              </textarea>
-            </div>
-            <button className="mt-5 bg-gray-900 text-white py-2 px-4 rounded hover:bg-blue-700" onClick={toggle}>Save Notes</button>
-          </div>
-          }
+          <button className="mt-5 bg-gray-900 text-white py-2 px-4 rounded hover:bg-blue-700" onClick={toggle}>
+            Save Notes
+          </button>
         </div>
-      );
+      )}
+    </div>
+  );
 }
